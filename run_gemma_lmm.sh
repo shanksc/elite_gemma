f832c55d1618a7bb045d71e5183345dedad5d59f
shanksc <cwshanks@ucsc.edu>
2025-03-13 17:16:22 -0700

    update lmm bash script

--- run_gemma_lmm.sh
+++ run_gemma_lmm.sh
@@ -6,5 +6,5 @@
 #SBATCH --time=12:00:00
 #SBATCH --partition=medium
 
-./gemma -bfile topmed_imputation/merged_maf_01 -k gemma_runs/merged_maf_01_grm.cXX.txt -lmm -outdir gemma_runs -o merged_maf_01_lmm_sex_covar -c gwas_prep/sex_covar.tsv
+./gemma -bfile topmed_imputation/merged_maf_01 -k gemma_runs/merged_maf_01_grm.cXX.txt -lmm -outdir gemma_runs -o merged_maf_01_lmm_sex_covar_v2 -c gwas_prep/sex_covar_one_hot.tsv -debug
 
